35748b0ad0a8ba90b1ba8efb40e08cafe4077363
ShahbazA1 <133510641+ShahbazA1@users.noreply.github.com>
2023-11-18 20:21:38 +0530

    Create TheRelationalOperators.java

---
+++ Graded Assignment on Introduction to Java/TheRelationalOperators.java
@@ -0,0 +1,16 @@
+public class assignment1 {
+    public static void main(String[] args) {
+        int num1 = 10;
+        int num2 = 20;
+        // boolean var1 = num1 == num2;
+        // boolean var2 = num1 != num2;
+        // boolean var3 = num1 > num2;
+        // boolean var4 = num1 < num2;
+
+        System.out.println("Equal:" + (num1 == num2));
+        System.out.println("Not Equal:" + ( num1 != num2));
+        System.out.println("num1 is Greater Than num2 : " + (num1 > num2));
+        System.out.println("num1 is Less Than num2 : " + (num1 < num2));
+
+    }
+}

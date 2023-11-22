f8e55464963b035342bdd94b5f54390fc041910e
ShahbazA1 <133510641+ShahbazA1@users.noreply.github.com>
2023-11-22 22:31:19 +0530

    UpdateManipulatingArrayUsingMapandFilterandReduce.java

--- Graded Assignment on Basic of JavaScript/ManipulatingArrayUsingMapandFilterandReduce.java
+++ Graded Assignment on Basic of JavaScript/ManipulatingArrayUsingMapandFilterandReduce.java
@@ -4,11 +4,13 @@ const customerData = [
   { customerID: 3, customerName: "Peter Jones", totalBillingAmount: 3200 },
 ];
 
+// Filter customers with total billing amount greater than 3000
 const eligibleCustomers = customerData.filter(customer => customer.totalBillingAmount > 3000);
 
-eligibleCustomers.forEach(customer => {
+// Iterate over eligible customers and print their details
+for (const customer of eligibleCustomers) {
   console.log(`Customer Name: ${customer.customerName}`);
   console.log(`Customer ID: ${customer.customerID}`);
   console.log(`Total Billing Amount: ${customer.totalBillingAmount}`);
-  console.log(`-------------------------------------`);
-});
+  console.log("---------------------------------------");
+}
